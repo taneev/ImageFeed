2f6782df894108c55e08790082d7904a8671f285
Timur Taneev <taneev@yandex.ru>
2023-05-20 21:48:27 +0200

    Верстка кодом экрана Профиль пользователя

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,29 +9,139 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
 
-    @IBOutlet weak var userpickImageView: UIImageView!
-    @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var useremailLabel: UILabel!
-    @IBOutlet weak var userBioLabel: UILabel!
+    private let leftMargin: CGFloat = 16
+    private let rightMargin: CGFloat = 16
 
+    private var profileImageView: UIImageView!
+    private var logoutButton: UIButton!
+
+    private var usernameLabel: UILabel?
+    private var emailLabel: UILabel?
+    private var bioLabel: UILabel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func logoutButtonTapped(_ sender: Any) {
+
+        addSubviewsAndConstraints()
+
+        // проверка верстки
+        usernameLabel?.text = "Екатерина Новикова"
+        emailLabel?.text = "@ekaterina_nov"
+        bioLabel?.text = "Hello, world!"
     }
 
-    /*
-    // MARK: - Navigation
+    private func addSubviewsAndConstraints() {
+        profileImageView = createProfileImageView()
+        view.addSubview(profileImageView)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        logoutButton = createLogoutButton()
+        view.addSubview(logoutButton)
+
+        usernameLabel = createUserNameLabel()
+        view.addSubview(usernameLabel!)
+
+        emailLabel = createEmailLabel()
+        view.addSubview(emailLabel!)
+
+        bioLabel = createBioLabel()
+        view.addSubview(bioLabel!)
+
+        addConstraints()
     }
-    */
+
+    private func createProfileImageView() -> UIImageView {
+        let userpickStub = UIImage(systemName: "person.crop.circle.fill")
+        let profileImageView = UIImageView(image: userpickStub)
+        profileImageView.tintColor = .ypGray
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileImageView)
+
+        return profileImageView
+    }
+
+    private func createLogoutButton() -> UIButton {
+        let buttonImage = UIImage(named: "ipad.and.arrow.forward")!
+        let button = UIButton.systemButton(with: buttonImage,
+                                           target: self,
+                                           action: #selector(Self.logoutButtonTapped))
+        button.tintColor = .ypRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+
+        return button
+    }
+
+    private func createUserNameLabel() -> UILabel {
+        let usernameLabel = UILabel()
+        usernameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        usernameLabel.textColor = .ypWhite
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(usernameLabel)
+        return usernameLabel
+    }
+
+    private func createEmailLabel() -> UILabel {
+        let emailLabel = UILabel()
+        emailLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        emailLabel.textColor = .ypGray
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emailLabel)
+        return emailLabel
+    }
+
+    private func createBioLabel() -> UILabel {
+        let bioLabel = UILabel()
+        bioLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        bioLabel.textColor = .ypWhite
+        bioLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bioLabel)
+        return bioLabel
+    }
+
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            profileImageView.heightAnchor.constraint(equalToConstant: 70),
+            profileImageView.widthAnchor.constraint(equalTo: profileImageView.heightAnchor),
+            profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftMargin),
+            profileImageView.topAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.topAnchor, constant: 40),
+
+            logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -rightMargin)
+        ])
+
+        if let usernameLabel {
+            NSLayoutConstraint.activate([
+                usernameLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+                usernameLabel.topAnchor.constraint(equalTo:  profileImageView.bottomAnchor, constant: 8),
+                usernameLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -rightMargin)
+            ])
+        }
+
+        if  let emailLabel,
+            let usernameLabel
+        {
+            NSLayoutConstraint.activate([
+                emailLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+                emailLabel.topAnchor.constraint(equalTo:  usernameLabel.bottomAnchor, constant: 8),
+                emailLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -rightMargin)
+            ])
+        }
+
+        if  let bioLabel,
+            let emailLabel
+        {
+            NSLayoutConstraint.activate([
+                bioLabel.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor),
+                bioLabel.topAnchor.constraint(equalTo:  emailLabel.bottomAnchor, constant: 8),
+                bioLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -rightMargin)
+            ])
+        }
+    }
+
+    @objc private func logoutButtonTapped() {
+        print("logout tapped")
+    }
+
 
 }
