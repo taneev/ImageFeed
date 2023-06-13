1784dbf0632be5bedc23ec93defe24ca2dec1769
Timur Taneev <taneev@yandex.ru>
2023-06-13 22:39:36 +0200

    SplashViewController теперь верстается только кодом

--- ImageFeed/SplashViewController/SplashViewController.swift
+++ ImageFeed/SplashViewController/SplashViewController.swift
@@ -12,11 +12,22 @@ import SwiftKeychainWrapper
 final class SplashViewController: UIViewController {
 
     private let tokenKey = "UnsplashBearerToken"
-    private let authViewControllerSegueID = "AuthViewControllerSegue"
     private let profileService = ProfileService.shared
+    private lazy var splashLogoImageView: UIImageView = {createSplashLogoImageView()}()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.backgroundColor = .ypBlack
+        view.addSubview(splashLogoImageView)
+
+        NSLayoutConstraint.activate(
+            [
+                splashLogoImageView.heightAnchor.constraint(equalToConstant: 75),
+                splashLogoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                splashLogoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ]
+        )
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -27,26 +38,28 @@ final class SplashViewController: UIViewController {
             profileFetch(token: token)
         }
         else {
-            performSegue(withIdentifier: authViewControllerSegueID, sender: self)
+            presentAuthViewController()
         }
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == authViewControllerSegueID {
-            guard let navigationController = segue.destination as? UINavigationController,
-                  let authViewController = navigationController.viewControllers[0] as? AuthViewController
-            else { fatalError("Segue \(authViewControllerSegueID) prepare error") }
-            authViewController.delegate = self
-        }
-        else {
-            super.prepare(for: segue, sender: sender)
-        }
+    private func createSplashLogoImageView() -> UIImageView {
+        let logoImage = UIImage(systemName: "practicumLogo")
+        let logoImageView = UIImageView(image: logoImage)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        return logoImageView
     }
 
     private func switchToTabBarController() {
-        guard let window = UIApplication.shared.windows.first else {fatalError("Invalid configuration")}
+        guard let window = UIApplication.shared.windows.first else {
+            assertionFailure("Invalid window configuration")
+            return
+        }
 
-        let tabBarViewController = storyboard?.instantiateViewController(withIdentifier: "TabBarViewController")
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        guard let tabBarViewController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as? TabBarController else {
+            assertionFailure("Could not instantiate TabBarController")
+            return
+        }
         window.rootViewController = tabBarViewController
     }
 
@@ -64,6 +77,18 @@ extension SplashViewController: AuthViewControllerDelegate {
         dismiss(animated: true)
     }
 
+    private func presentAuthViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        guard let authViewController = storyboard.instantiateViewController(withIdentifier: "authViewController") as? AuthViewController
+        else {
+            assertionFailure("AuthViewController couldn't be initialized")
+            return
+        }
+        authViewController.delegate = self
+        authViewController.modalPresentationStyle = .fullScreen
+        present(authViewController, animated: true)
+    }
+
     private func alertAndSwitchToAuthViewController() {
         let alert = AlertModel(title: "Что-то пошло не так(",
                                message: "Не удалось войти в систему",
@@ -71,17 +96,17 @@ extension SplashViewController: AuthViewControllerDelegate {
         let alertPresenter = AlertPresenter(controller: self)
         alertPresenter.showAlert(alert: alert) {[weak self] _ in
             guard let self else {return}
-            performSegue(withIdentifier: self.authViewControllerSegueID, sender: self)
+            presentAuthViewController()
         }
     }
 
     func profileFetch(token: String) {
-        profileService.fetchProfile(token) { [weak self] result in
+        profileService.fetchProfile(token) {[weak self] result in
             guard let self else {return}
 
             switch result {
             case .success(let profile):
-                // бросаем вдогонку запрос чтения url аватара, но не ждем здесь ответа
+                // бросаем вдогонку запрос чтения url аватара, но не ждем здесь ответа (и ошибок)
                 ProfileImageService.shared.fetchProfileImageURL(token, username: profile.username) { _ in }
 
                 UIBlockingProgressHUD.dismiss()
