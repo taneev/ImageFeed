1b5ebc26c0e7cd3da77148e7acb079e287797691
Timur Taneev <taneev@yandex.ru>
2023-07-09 23:42:58 +0300

    настройка индикатора активности

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import ProgressHUD
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        ProgressHUD.animationType = .systemActivityIndicator
+        ProgressHUD.colorHUD = .ypBlack
+        ProgressHUD.colorAnimation = .lightGray
         return true
     }
 
